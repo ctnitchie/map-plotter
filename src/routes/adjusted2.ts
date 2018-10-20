aa6ec323621335be8e790467ee92f66dca1873d5
Chris Nitchie <cnitchie@gmail.com>
2018-10-20 07:20:45 -0400

    Cleaning up routes

--- src/routes/adjusted2.ts
+++ src/routes/adjusted2.ts
@@ -10,18 +10,21 @@ const westLine = 30;
 const mid = plot.addRoute(null, baseLine, 134, 'Base Midpoint', {makeDot: false, label: routeLabeler});
 const dino = plot.addRoute(mid, 355, 85, 'Scubasaurus', {label: routeLabeler});
 const buddha = plot.addRoute(dino, 330, 51, 'SinkaBuddha', {label: routeLabeler});
-const david = plot.addRoute(buddha, eastLine, 70, 'David Point', {labelDot: false, makeDot: false, label: routeLabeler});
-const soldier = plot.addRoute(david, eastLine, 58, 'Soldier Point', {labelDot: false, makeDot: false, label: routeLabeler});
+const david = plot.addRoute(buddha, eastLine, 70, 'El Dudarino', {labelDot: false, makeDot: false, label: routeLabeler});
+const soldier = plot.addRoute(david, eastLine, 58, 'Brazierian Soldier', {labelDot: false, makeDot: false, label: routeLabeler});
 const platformReturn = plot.addRoute(soldier, eastLine, 46, 'Platform NW', {labelDot: false, label: routeLabeler});
-const platNE = plot.addRoute(platformReturn, 82, 16, '', {label: () => '', makeDot: false});
-const platSE = plot.addRoute(platNE, 172, 10, '', {label: () => '', makeDot: false});
-plot.addRoute(platSE, 262, 16, '', {label: () => '', makeDot: false});
 const lady = plot.addRoute(platformReturn, 325, 19, 'Lady of the Lake', {showLabel: false});
 const dome = plot.addRoute(lady, 0, 63, 'Dome', {label: routeLabeler});
 plot.addRoute(dome, 145, 30, 'Mirror', {type: LineType.DASHED, label: routeLabeler});
+plot.addRoute(lady, 24, 45, 'Mirror', {makeDot: false, type: LineType.DASHED, label: routeLabeler});
 
+// West platform square
+const platNE = plot.addRoute(platformReturn, 82, 16, '', {label: () => '', makeDot: false});
+const platSE = plot.addRoute(platNE, 172, 10, '', {label: () => '', makeDot: false});
+plot.addRoute(platformReturn, 172, 10, '', {label: () => '', makeDot: false});
+plot.addRoute(platSE, 262, 16, '', {label: () => '', makeDot: false});
 
-plot.addRoute(lady, 24, 45, 'Mirror from Lady', {makeDot: false, type: LineType.DASHED, label: routeLabeler});
+// Misc. lines
 plot.addRoute(david, 45, 5, 'El Dudarino', {type: LineType.NONE});
 plot.addRoute(soldier, 45, 5, 'Brazierian Soldier', {type: LineType.NONE});
 plot.addRoute(mid, 25, 10, 'HMCS Brynne Wreck', {type: LineType.NONE});
@@ -29,26 +32,28 @@ plot.addRoute(mid, 175, 6, 'Mid-line anchor', {makeDot: false, label: nullLabele
 
 const pvcPipe = plot.addRoute(mid, baseLine, 75, 'PVC Pipe', {label: routeLabeler});
 const westPlatform = plot.addRoute(pvcPipe, baseLine, 67, 'West Platform', {label: routeLabeler});
-const westPlatformWest = plot.addRoute(westPlatform, 270, 9, 'West Platform Far Corner', {showLabel: false, labelDot: false});
-const hoop1 = plot.addRoute(westPlatformWest, 315, 64, 'Hoop B');
-plot.addRoute(hoop1, 315, 50, 'Trash');
+const westPlatformWest = plot.addRoute(westPlatform, 270, 9, 'West Platform West Corner', {showLabel: false, labelDot: false});
 const hoop2 = plot.addRoute(westPlatformWest, 335, 21, 'Hoop A');
 plot.addRoute(hoop2, 335, 113, 'End of the Line');
-const hoop3 = plot.addRoute(westPlatform, westLine, 67, 'Hoop D Line', {makeDot: false, label: routeLabeler});
-plot.addRoute(hoop3, westLine, 139, 'Buddah (return)', {makeDot: false, label: routeLabeler});
+const hoop1 = plot.addRoute(westPlatformWest, 315, 64, 'Hoop B');
+plot.addRoute(hoop1, 315, 50, 'Trash');
+const hoop3 = plot.addRoute(westPlatform, westLine, 67, 'Hoop D', {makeDot: false, label: routeLabeler});
+plot.addRoute(hoop3, westLine, 139, 'Buddah', {makeDot: false, label: routeLabeler});
 
 hoopRoute(plot, hoop2, false);
 
-const wpSW = plot.addRoute(westPlatformWest, 180, 9, 'West Platform SW', {makeDot: false, label: nullLabeler});
-const wpSE = plot.addRoute(wpSW, 90, 9, 'West Platform SE', {makeDot: false, label: nullLabeler});
-plot.addRoute(wpSE, 0, 9, 'West Platform NE', {makeDot: false, label: nullLabeler});
+// West platform square
+const wpSW = plot.addRoute(westPlatformWest, 180, 9, '', {makeDot: false, label: nullLabeler});
+const wpSE = plot.addRoute(wpSW, 90, 9, '', {makeDot: false, label: nullLabeler});
+plot.addRoute(wpSE, 0, 9, '', {makeDot: false, label: nullLabeler});
 
+// Shelter routes
 const shelter = plot.addRoute(null, 160, 265, 'Shelter', {type: LineType.DASHED, labelDot: false, label: routeLabeler});
 const boat = plot.addRoute(shelter, 270, 122, 'Boat', {type: LineType.DASHED, labelDot: false, label: routeLabeler});
 plot.addRoute(boat, 265, 297, '', {type: LineType.DASHED, label: routeLabeler});
 
 // Draw the East platform
-plot.addConnector(null, platformReturn);
+//plot.addConnector(null, platformReturn);
 
 export function hoopRoute(plot: MapPlot, from: Route, label: boolean): void {
   const lblFn = label ?routeLabeler : nullLabeler;
